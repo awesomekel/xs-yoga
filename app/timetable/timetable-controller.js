8c3013d4ce5c485ae485a4a69e16aadae0571497
Raphael HUANG <raphael.huang@d1m.cn>
2017-02-27 01:08:46 +0800

    Implemented controller logic for timetable.

--- app/timetable/timetable-controller.js
+++ app/timetable/timetable-controller.js
@@ -15,5 +15,70 @@
   function TimetableCtrl() {
     var vm = this;
     vm.ctrlName = 'TimetableCtrl';
+    vm.selected = 'monday';
+    vm.select = function (day) {
+      vm.selected = day;
+      vm.selectedTimetable = vm.timetable[day];
+    };
+
+    vm.days = [
+      'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'
+    ];
+    vm.isSelected = function (day) {
+      return vm.selected === day;
+    };
+    vm.timetable = {
+      monday: [
+        {
+          time: '6:20 p.m. - 7:20p.m.',
+          class: 'Aerial Yoga Beginner Mon',
+          instructor: 'Sakina'
+        }
+      ],
+      tuesday: [
+        {
+          time: '6:20 p.m. - 7:20p.m.',
+          class: 'Aerial Yoga Beginner',
+          instructor: 'Sakina'
+        }
+      ],
+      wednesday: [
+        {
+          time: '6:20 p.m. - 7:20p.m.',
+          class: 'Aerial Yoga Beginner',
+          instructor: 'Sakina'
+        }
+      ],
+      thursday: [
+        {
+          time: '6:20 p.m. - 7:20p.m.',
+          class: 'Aerial Yoga Beginner',
+          instructor: 'Sakina'
+        }
+      ],
+      friday: [
+        {
+          time: '6:20 p.m. - 7:20p.m.',
+          class: 'Aerial Yoga Beginner',
+          instructor: 'Sakina'
+        }
+      ],
+      saturday: [
+        {
+          time: '6:20 p.m. - 7:20p.m.',
+          class: 'Aerial Yoga Beginner',
+          instructor: 'Sakina'
+        }
+      ],
+      sunday: [
+        {
+          time: '6:20 p.m. - 7:20p.m.',
+          class: 'Aerial Yoga Beginner',
+          instructor: 'Sakina'
+        }
+      ]
+    };
+
+    vm.select('monday');
   }
 }());
